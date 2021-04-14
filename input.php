7e76184911f6fa77c1334112d82b93d1b5cb0151
bagusghifari <82530696+bagusghifari@users.noreply.github.com>
2021-04-14 23:30:37 +0700

    Add files via upload

---
+++ input.php
@@ -0,0 +1,10 @@
+<?php
+    $nama = $_POST ['nama'];
+    $nomortelepon = $_POST ['nomortelepon'];
+    $email = $_POST ['email'];
+    $gender = $_POST ['gender'];
+
+
+        echo "Data yang anda masukan sudah kami simpan, Terimakasih";
+
+?>
